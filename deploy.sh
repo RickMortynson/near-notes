7f1bd0fe7275a292c9197016a83ca9435fc5ce12
Taras Bratyshchenko <taras.bratyshchenko@gmail.com>
2022-06-03 00:03:03 +0300

    update script for github actions; update ci.yml

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir out
 near deploy --wasmFile ./out/near_todo_categories.wasm --accountId categories.unicorny.testnet
 near deploy --wasmFile ./out/near_todo_tasks.wasm --accountId notes.unicorny.testnet
