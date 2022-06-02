5146e2c8d76788968cd585df8c35e2d53f799cd4
Taras Bratyshchenko <taras.bratyshchenko@gmail.com>
2022-06-03 00:08:02 +0300

    update build.sh - fix pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir out
 near deploy --wasmFile ./out/near_todo_categories.wasm --accountId categories.unicorny.testnet
 near deploy --wasmFile ./out/near_todo_tasks.wasm --accountId notes.unicorny.testnet
