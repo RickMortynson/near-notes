7cacb5e1973f43cf16c358acf56eb947841f1837
Taras Bratyshchenko <taras.bratyshchenko@gmail.com>
2022-06-02 22:59:58 +0300

    fix: edit deploy.sh, workflow names

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+near deploy --wasmFile ./out/near_todo_categories.wasm --accountId categories.unicorny.testnet
+near deploy --wasmFile ./out/near_todo_tasks.wasm --accountId notes.unicorny.testnet
